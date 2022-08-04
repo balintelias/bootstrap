d1454e65986f392df1995006054bc24d87e33981
Elias <ebalint896@gmail.com>
2022-08-04 12:05:23 +0200

    cli argument parsing and error handling

--- source/main.py
+++ source/main.py
@@ -1,3 +1,5 @@
+import getopt, sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 import generate
@@ -5,10 +7,49 @@ import bootstrap
 import visual
 import retrieve
 
+help_msg = """Usage:
+-h --Help   
+-o: --Output=
+
+The program requires the input file to be named dataset.txt and to be placed in the same directory.
+"""
+
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+
+# Options
+options = "ho:"
+ 
+# Long options
+long_options = ["Help", "Output="]
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+     
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+ 
+        if currentArgument in ("-h", "--Help"):
+            print (help_msg)
+            sys.exit("Program exiting")
+             
+        elif currentArgument in ("-o", "--Output"):
+            print (("Enabling special output mode (% s)") % (currentValue))
+             
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+
 """
 The final script will retrieve the array instead of generating it
 """
-arr = retrieve.func_retrieve()
+try:
+    arr = retrieve.func_retrieve()
+except:
+    print("Error while retrieving input: dataset.txt not found")
+    sys.exit("Program exiting")
 
 visual.func_visual(arr)
 
