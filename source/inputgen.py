59117aa613fa53df72f3d201e402e5d20d34b6e1
Elias <ebalint896@gmail.com>
2022-08-11 16:22:25 +0200

    comments

--- source/inputgen.py
+++ source/inputgen.py
@@ -2,26 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import generate
 
+# generate two dimensional input array
 arr = generate.func_generate()
 
-print(str(arr))
-
+# write to file
 file1 = open("input.csv", "w")  # write mode
-
-
 arr_size = len(arr)
 count = 0
-
 while (count < arr_size):
 	content = np.array(arr)[count]
 	file1.write(str(content))
 	file1.write(" ")
 	count = count + 1
-
 file1.close()
 
-file5 = open("input.csv", "r")
-#read input to file
+# separate values by commas
+file5 = open("input.csv", "r")  #read mode
 content = file5.read()
 file5.close()
 
@@ -34,6 +30,5 @@ content = content[:-1]
 content = content.replace(' ',',')
 
 file6 = open("input.csv", "w")  # write mode
-#write output into file
 file6.write(content)
 file6.close()
